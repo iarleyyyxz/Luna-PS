9b9a4600d2681c8e4ffdf46fe30e2bc114579490
iarleyyyxz <josy-@JOSSIANE>
2025-07-03 20:16:04 -0300

    Vertex (x, y) attrib

--- Luna/GPU/Vertex2D.cs
+++ Luna/GPU/Vertex2D.cs
@@ -2,15 +2,14 @@ namespace Luna.Math
 {
     public struct Vertex2D
     {
-        private float x, y;
-
-        public float X { get => x; set => x = value; }
-        public float Y { get => y; set => y = value; }
+        public float X, Y;
 
         public Vertex2D(float x, float y)
         {
-            this.x = x;
-            this.y = y;
+            X = x;
+            Y = y;
         }
+
+        public override string ToString() => $"({X},{Y})";
     }
 }
